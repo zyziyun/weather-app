6492dedf3d7cc502318b5dc0db44c24515671f15
Yun Zi <yzi1@hawk.iit.edu>
2023-04-21 12:16:51 -0500

    fix: eslint error

--- src/services/request.js
+++ src/services/request.js
@@ -1,7 +1,7 @@
 export const APP_KEY = '8fab8af74de5a18abcdf810ee58ac4e8';
 
 export const buildUrlParams = (url, params) => {
-    if (Object.keys(params).length == 0) return url;
+    if (Object.keys(params).length === 0) return url;
     return url + Object.keys(params).reduce((prev, curr) => {
         return `${prev}&${curr}=${params[curr]}`;
     }, '?');
